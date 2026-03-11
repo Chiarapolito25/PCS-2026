b3400ceb59d45e2aa8abf9041cbf4d98a4068c83
Chiarapolito25 <tumminochiara@gmail.com>
2026-03-11 14:55:00 +0000

    esercitazione 1

---
+++ esercitazione1/funzione1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+
+int main(int argc, const char *argv[]){
+string filename=argv[1];
+ifstream ifs(filename);
+
+if ( ifs.is_open() ) { 
+	for (int i=0; i <=3; i++) {
+			string location;
+			double temp1;
+			double temp2;
+			double temp3;
+			double temp4;
+			
+			if (ifs >> location >> temp1 >> temp2 >> temp3 >> temp4){
+				double mean_temp=(temp1+temp2+temp3+temp4)/4.0;
+				cout << location << " : " << mean_temp << "\n";
+			}
+		
+	}
+}
+
+return 0;
+}
